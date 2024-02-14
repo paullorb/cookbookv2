59ab38627694f5f1d23c54203914f992f4cec686
paullorb <port19@protonmail.com>
2024-02-14 16:50:52 +0100

    api connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from 'react';
+import './App.css';
+import { createClient} from 'contentful';
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [recipes, setRecipes] = useState([]);
+
+  const client = createClient({
+    space: "mwoz8j7lspjq",
+    accessToken: "7d4TEO9tdvluAn_KIRYM_jcoyDImg9rZcuS4HxrGbuc"
+  });
+
+  useEffect (() => {
+    const getRecipes = async () => {
+      const entryItems = await client.getEntries()
+      setRecipes(entryItems.items)
+      console.log(entryItems.items)
+    }
+
+    getRecipes();
+
+  }, [])
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Cookbookv2</h1>
+      <br />
+      {recipes.map((recipe) => (
+        <div key={recipe.sys.id}>
+          <h1>{recipe.fields.recipeTitle}</h1>
+          <img 
+          style={{width: "200px", height: "200px"}}
+          src={recipe.fields.recipePicture.fields.file.url} 
+          alt={recipe.fields.recipePicture.fields.file.fileName}
+          />
+          {recipe.fields.ingredients.map((el, index) => (
+            <ul key={index}>
+              <li>{el}</li>
+            </ul>
+          ))}
+
+        </div>
+      ))}
     </>
   )
 }
