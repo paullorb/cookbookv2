81142daf2d8bd489dd1e486be25d75e2b5a3dd82
Moody458 <morentrop@gmail.com>
2024-03-08 15:51:21 +0100

    final changes

--- src/components/LatestRecipes.jsx
+++ src/components/LatestRecipes.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import style from "./LatestRecipes.module.css";
+import { Link } from "react-router-dom";
 
 function LatestRecipes() {
   const [allRecipes, setAllRecipes] = useState(["Loading recipes..."]);
@@ -11,7 +12,7 @@ function LatestRecipes() {
         .then((data) => {
           setAllRecipes(data);
           // console.log(data);
-        })
+        });
     } catch (error) {
       console.error("Error fetching recipes:", error);
     }
@@ -27,23 +28,28 @@ function LatestRecipes() {
         <h2>Latest Recipes</h2>
       </div>
       <div className={style.container}>
-      {allRecipes.slice(-3).map((recipe) => (
-        <div key={recipe.id} className={style.recipe}>
-          <div className={style.textContainer}>
-            <h3 className={style.heading}>{recipe.recipetitle}</h3>
+        {allRecipes.slice(-3).map((recipe) => (
+          <div key={recipe.id} className={style.recipe}>
+            <div className={style.textContainer}>
+              <Link key={recipe.id} to={`/AllRecipes/${recipe.id}`}>
+                <h3 className={style.heading}>{recipe.recipetitle}</h3>
+              </Link>
+            </div>
+            <div className={style.textContainer}>
+              <p className={style.description}>{recipe.description}</p>
+            </div>
+            <div className={style.textContainer}>
+              <p className={style.category}>{recipe.category}</p>
+            </div>
+            <p className={style.preptime}>{recipe.preptime}</p>
+            <img
+              src={recipe.picture_url}
+              alt={recipe.recipetitle}
+              className={style.img}
+            />
           </div>
-          <div className={style.textContainer}>
-            <p className={style.description}>{recipe.description}</p>
-          </div>
-          <div className={style.textContainer}>
-            <p className={style.category}>{recipe.category}</p>
-          </div>
-          <p className={style.preptime}>{recipe.preptime}</p>
-          <img src={recipe.picture_url} alt={recipe.recipetitle} className={style.img}/>
-        </div>
-))}
+        ))}
       </div>
-
     </section>
   );
 }
